cf9694cecccc02755d75e9d389bb3cb49939ee18
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-18 16:23:13 -0400

    Add passing test for ItemConstructor_CreatesInstanceOfItem_Item()

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests
+  {
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+  }
+}
