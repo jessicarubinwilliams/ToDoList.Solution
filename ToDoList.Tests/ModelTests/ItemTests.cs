2dcd36362c206c915cb91853f233ba9a90097101
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-18 18:28:51 -0400

    Add passing test for GetAll_ReturnsEmptyList_ItemList()

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,12 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
 
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+    
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
@@ -32,6 +39,14 @@ namespace ToDoList.Tests
       newItem.Description = updatedDescription;
       string result = newItem.Description;
       Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> {};
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
--- ToDoList/Models/Items.cs
+++ ToDoList/Models/Items.cs
@@ -1,12 +1,25 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
     public string Description { get; set; }
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    private static List<Item> _instances = new List<Item> {};
 
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this);
     }
   }
 }
