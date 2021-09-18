3767bf84283d6197c8abfae7507bc463570912a7
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-18 17:02:20 -0400

    Add passing test for SetDescription_SetDescription_String()

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -17,11 +17,21 @@ namespace ToDoList.Tests
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
-      string description = "Walk the dog.";
+      string description = "Walk the dog";
       Item newItem = new Item(description);
       string result = newItem.Description;
       Assert.AreEqual(description, result);
     }
 
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog";
+      Item newItem = new Item(description);
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+      Assert.AreEqual(updatedDescription, result);
+    }
   }
 }
