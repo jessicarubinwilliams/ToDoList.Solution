044146936e6ad393bfb54f2f4bd60eb18e744cbd
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-23 18:49:44 -0400

    Add Controller and View for a splash page

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
