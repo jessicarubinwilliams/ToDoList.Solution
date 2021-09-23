cb35528d71793a9e69a4d2ec04ec50e1fab21ed5
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-23 18:37:30 -0400

    Move code for existing controllers to proper file

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return RedirectToAction("Index");
+    }
+  }
+}
