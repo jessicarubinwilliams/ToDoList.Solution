6a712ac95b5216e9e3a36fce497a7b4e75af81ab
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-25 16:45:25 -0400

    Add route that handles adding new categories

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -19,5 +19,12 @@ namespace ToDoList.Controllers
         {
           return View();
         }
+
+        [HttpPost("categories")]
+        public ActionResult Create(string categoryName)
+        {
+          Category newCategory = new Category(categoryName);
+          return RedirectToAction("Index");
+        }
     }
 }
