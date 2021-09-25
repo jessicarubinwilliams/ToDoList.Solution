aa8802f4cecda5a060515c61fb0fdb37ba28576c
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-25 14:23:14 -0400

    Add good fail of test Find_ReturnsCorrectCategory_Category()

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -50,5 +50,16 @@ namespace ToDoList.Tests
       List<Category> result = Category.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectCategory_Category()
+    {
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+      Category result = Category.Find(2);
+      Assert.AreEqual(newCategory2, result);
+    }
   }
 }
