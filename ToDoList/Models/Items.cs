352da63e2e304334f231e799270e3a6bb3cc01b7
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-27 22:29:26 -0400

    Updat GetAll() to get list of item instances from database

--- ToDoList/Models/Items.cs
+++ ToDoList/Models/Items.cs
@@ -12,10 +12,33 @@ namespace ToDoList.Models
     {
       Description = description;
     }
+    public Item(string description, int id)
+    {
+      Description = description;
+      Id = id;
+    }
 
     public static List<Item> GetAll()
     {
-      return _instances;
+      List<Item> allItems = new List<Item> { };
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+          int itemId = rdr.GetInt32(0);
+          string itemDescription = rdr.GetString(1);
+          Item newItem = new Item(itemDescription, itemId);
+          allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+          conn.Dispose();
+      }
+      return allItems;
     }
     public static void ClearAll()
     {
