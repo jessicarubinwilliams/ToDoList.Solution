6d938bc795a471bd43431f70af74751895567ef6
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-10 17:53:51 -0400

    Update Create route to include many-to-many relationship

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -21,19 +21,24 @@ namespace ToDoList.Controllers
       return View(_db.Items.ToList());
     }
 
-//     public ActionResult Create()
-//     {
-//         ViewBag.Categories = _db.Categories.ToList();
-//         return View();
-//     }
+    public ActionResult Create()
+    {
+        ViewBag.Categories = _db.Categories.ToList();
+        return View();
+    }
 
-//     [HttpPost]
-//     public ActionResult Create(Item item)
-//     {
-//         _db.Items.Add(item);
-//         _db.SaveChanges();
-//         return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+        _db.Items.Add(item);
+        _db.SaveChanges();
+        if (CategoryId != 0)
+        {
+          _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+        }
+    _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
 
     public ActionResult Details(int id)
     {
