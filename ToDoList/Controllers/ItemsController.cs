5e47b7ffa4a4b1fda9697923d61e8d82d32dbfa3
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-10 18:10:49 -0400

    Update Edit post route to reflect many-to-many relationship

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -49,20 +49,24 @@ namespace ToDoList.Controllers
         return View(thisItem);
     }
 
-//     public ActionResult Edit(int id)
-//     {
-//         var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-//          ViewBag.Categories = _db.Categories.ToList();
-//         return View(thisItem);
-//     }
+    public ActionResult Edit(int id)
+    {
+        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+         ViewBag.Categories = _db.Categories.ToList();
+        return View(thisItem);
+    }
 
-//     [HttpPost]
-//     public ActionResult Edit(Item item)
-//     {
-//         _db.Entry(item).State = EntityState.Modified;
-//         _db.SaveChanges();
-//         return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Edit(Item item, int CategoryId)
+    {
+        if (CategoryId != 0)
+        {
+          _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+        }
+        _db.Entry(item).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
 
 //     public ActionResult Delete(int id)
 //     {
