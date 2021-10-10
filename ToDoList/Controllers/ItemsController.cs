13edacff96ee3ebab964f610928fafc8c9463d33
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-10 17:33:22 -0400

    Update Index and Details route for many-to-many relationship

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -18,58 +18,60 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.Include(item => item.Category).ToList();
-      return View(model);
+      return View(_db.Items.ToList());
     }
 
-    public ActionResult Create()
-    {
-        ViewBag.Categories = _db.Categories.ToList();
-        return View();
-    }
+//     public ActionResult Create()
+//     {
+//         ViewBag.Categories = _db.Categories.ToList();
+//         return View();
+//     }
 
-    [HttpPost]
-    public ActionResult Create(Item item)
-    {
-        _db.Items.Add(item);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-    }
+//     [HttpPost]
+//     public ActionResult Create(Item item)
+//     {
+//         _db.Items.Add(item);
+//         _db.SaveChanges();
+//         return RedirectToAction("Index");
+//     }
 
     public ActionResult Details(int id)
     {
-        Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+        var thisItem = _db.Items
+          .Include(item => item.JoinEntities)
+          .ThenInclude(join => join.Category)
+          .FirstOrDefault(item => item.ItemId == id);
         return View(thisItem);
     }
 
-    public ActionResult Edit(int id)
-    {
-        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-         ViewBag.Categories = _db.Categories.ToList();
-        return View(thisItem);
-    }
+//     public ActionResult Edit(int id)
+//     {
+//         var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+//          ViewBag.Categories = _db.Categories.ToList();
+//         return View(thisItem);
+//     }
 
-    [HttpPost]
-    public ActionResult Edit(Item item)
-    {
-        _db.Entry(item).State = EntityState.Modified;
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-    }
+//     [HttpPost]
+//     public ActionResult Edit(Item item)
+//     {
+//         _db.Entry(item).State = EntityState.Modified;
+//         _db.SaveChanges();
+//         return RedirectToAction("Index");
+//     }
 
-    public ActionResult Delete(int id)
-    {
-        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-        return View(thisItem);
-    }
+//     public ActionResult Delete(int id)
+//     {
+//         var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+//         return View(thisItem);
+//     }
 
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-        _db.Items.Remove(thisItem);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-    }
+//     [HttpPost, ActionName("Delete")]
+//     public ActionResult DeleteConfirmed(int id)
+//     {
+//         var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+//         _db.Items.Remove(thisItem);
+//         _db.SaveChanges();
+//         return RedirectToAction("Index");
+//     }
   }
 }
