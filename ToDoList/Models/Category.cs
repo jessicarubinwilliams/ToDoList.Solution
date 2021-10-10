aa550dffb21841f8fb776c3df0bdcfc153fc3356
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-10 15:30:27 -0400

    Update model for many-to-many relationship & Code First Migration

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -6,11 +6,11 @@ namespace ToDoList.Models
   {
     public Category()
       {
-        this.Items = new HashSet<Item>();
+        this.JoinEntities = new HashSet<CategoryItem>();
       }
 
       public int CategoryId { get; set; }
       public string Name { get; set; }
-      public virtual ICollection<Item> Items { get; set; }
+      public virtual ICollection<CategoryItem> JoinEntities { get; set; }
     }
 }
