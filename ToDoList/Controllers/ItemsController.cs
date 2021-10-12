ed1b2fe3b121257282f210992690f5c1571c1b1f
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-12 14:17:53 -0400

    Uncomment delete routes now that many-to-many functionality is in place elsewhere

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -86,19 +86,19 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
-//     public ActionResult Delete(int id)
-//     {
-//         var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-//         return View(thisItem);
-//     }
+    public ActionResult Delete(int id)
+    {
+        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+        return View(thisItem);
+    }
 
-//     [HttpPost, ActionName("Delete")]
-//     public ActionResult DeleteConfirmed(int id)
-//     {
-//         var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-//         _db.Items.Remove(thisItem);
-//         _db.SaveChanges();
-//         return RedirectToAction("Index");
-//     }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+        _db.Items.Remove(thisItem);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
   }
 }
