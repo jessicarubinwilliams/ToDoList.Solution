496b5fd31aa7396b576f324454f15ccddacd1099
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-24 12:49:13 -0700

    Add property that assoicates items to a specific user
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>
    Co-authored-by: Amerik Velasquez <amerik.velasquez22@gmail.com>

--- ToDoList/Models/Items.cs
+++ ToDoList/Models/Items.cs
@@ -10,7 +10,8 @@ namespace ToDoList.Models
     }
     public int ItemId { get; set; }
     public string Description { get; set; }
-    
+    public virtual ApplicationUser User { get; set; }
+
     public virtual ICollection<CategoryItem> JoinEntities { get;}
   }
 }
